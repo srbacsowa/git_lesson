309076abce29a991cf0681e039ea625aaade0ce2
Stephen Bacso <stephen.bacso@gmail.com>
2017-09-03 13:39:36 -0400

    Update variable name
    Changed all x's to z's.

--- src/square.py
+++ src/square.py
@@ -1,10 +1,10 @@
 
-def my_square(x):
+def my_square(z):
     """
-        Return the square of the number x.
+        Return the square of the number z.
         Use the ** operator to square.
     """
-    return x ** 2
+    return z ** 2
 
 print(my_square(4))
 
