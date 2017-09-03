5492754aae10c0627a33bff0eb159abd2ece47f5
srbacso <stephen.bacso@gmail.com>
2017-09-03 13:38:50 -0400

    Added a comment and changed the square operator

--- src/square.py
+++ src/square.py
@@ -3,9 +3,10 @@ def my_square(x):
     """
         Return the square of the number x.
         Use the ** operator to square.
+        Add another comment.
     """
-    return x ** 2
+    return x * x
 
-print(my_square(4))
+print(my_square(12))
 
 
